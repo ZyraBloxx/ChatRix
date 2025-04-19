683ba62698415f105aab511c1be61cd05b5593c4
ZyraBloxx <s.leeuwenkamp.34131@gmail.com>
2025-04-19 09:57:24 +0200

    Add files via upload

---
+++ app.js
@@ -0,0 +1,7 @@
+
+window.onload = function () {
+  setTimeout(() => {
+    document.getElementById("boot-screen").style.display = "none";
+    document.getElementById("main-ui").style.display = "flex";
+  }, 3000);
+};
